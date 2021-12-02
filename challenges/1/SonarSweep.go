420b014b0637eacf43bb30a43d1be5bd4419a0a6
Ross MacLean <ross@rossmac.co.uk>
2021-12-02 13:28:52 +0000

    Complete problem 1 part 2.

--- challenges/1/SonarSweep.go
+++ challenges/1/SonarSweep.go
@@ -7,11 +7,16 @@ import (
 
 func main() {
 	depths := utils.ReadFileToIntSlice("challenges/1/input")
-	numIncreases := getNumberOfIncreases(depths)
-	log.Printf("Number of increases %d", numIncreases)
+
+	numIncreasesIndividual := getNumberOfIncreasesIndividual(depths)
+	log.Printf("Number of increases individual %d", numIncreasesIndividual)
+
+	numToSum := 3
+	numIncreasesSum := getNumberOfIncreasesSum(depths, numToSum)
+	log.Printf("Number of increases sum using %d values %d", numToSum, numIncreasesSum)
 }
 
-func getNumberOfIncreases(depths []int) int {
+func getNumberOfIncreasesIndividual(depths []int) int {
 	var increases = 0
 	for i := range depths {
 		if i == 0 {
@@ -22,4 +27,25 @@ func getNumberOfIncreases(depths []int) int {
 		}
 	}
 	return increases
+}
+
+func getNumberOfIncreasesSum(depths []int, numToSum int) int {
+	var increases = 0
+	for i := 0; i < len(depths)-numToSum; i++ {
+		firstSum := getSum(depths, i, numToSum)
+		secondsSum := getSum(depths, i+1, numToSum)
+
+		if secondsSum > firstSum {
+			increases++
+		}
+	}
+	return increases
+}
+
+func getSum(depths []int, startIndex int, numToSum int) int {
+	var sum = 0
+	for i := 0; i < numToSum; i++ {
+		sum += depths[startIndex+i]
+	}
+	return sum
 }
--- challenges/1/SonarSweep_test.go
+++ challenges/1/SonarSweep_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func Test_getNumberOfIncreases(t *testing.T) {
+func Test_getNumberOfIncreasesIndividual(t *testing.T) {
 	type args struct {
 		depths []int
 	}
@@ -12,15 +12,43 @@ func Test_getNumberOfIncreases(t *testing.T) {
 		want int
 	}{
 		{
-			name: "Example Data",
+			name: "Get number of increases individual",
 			args: args{[]int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}},
 			want: 7,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getNumberOfIncreases(tt.args.depths); got != tt.want {
-				t.Errorf("getNumberOfIncreases() = %v, want %v", got, tt.want)
+			if got := getNumberOfIncreasesIndividual(tt.args.depths); got != tt.want {
+				t.Errorf("getNumberOfIncreasesIndividual() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getNumberOfIncreasesSum(t *testing.T) {
+	type args struct {
+		depths   []int
+		numToSum int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Get number of increases sum",
+			args: args{
+				depths:   []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263},
+				numToSum: 3,
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getNumberOfIncreasesSum(tt.args.depths, tt.args.numToSum); got != tt.want {
+				t.Errorf("getNumberOfIncreasesSum() = %v, want %v", got, tt.want)
 			}
 		})
 	}
